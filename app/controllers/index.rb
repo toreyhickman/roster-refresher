cc46e455398a9d0019c47b6eed8ce8e512189792
Torey Hickman <torey@toreyhickman.com>
2014-01-11 20:51:51 -0500

    Routes for form submission
    Data from the form on the index page is sent to the (post '/update_spreadsheet') controller, which will update the spreadsheet and redirect to ('/updated').

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,3 +6,17 @@ get '/' do
   @cohorts = recent_chicago_cohorts
   erb :index
 end
+
+get '/updated' do
+  erb :updated
+end
+
+post '/update_spreadsheet' do
+  selected_cohorts = recent_chicago_cohorts.select { |cohort| params[:cohorts].include?(cohort.name) }
+
+  selected_cohorts.each do |cohort|
+    puts cohort.students.count
+  end
+
+  redirect to '/updated'
+end
