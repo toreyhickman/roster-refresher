a031c3b4a8d0792ccaf7fb1cadd11499cc5c075e
Torey Hickman <torey@toreyhickman.com>
2014-09-15 15:47:16 -0500

    Stop loading environment variables for YAML

--- config/environment.rb
+++ config/environment.rb
@@ -32,13 +32,3 @@ APP_NAME = APP_ROOT.basename.to_s
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
-
-# Set up environment variables
-require 'yaml'
-
-env_file = File.join("#{APP_ROOT}/config/application.yml")
-
-YAML.load(File.open(env_file)).each do |key, value|
-  ENV[key.to_s] = value
-end if File.exists?(env_file)
-
